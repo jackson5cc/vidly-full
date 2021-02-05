f09940fb839e902ce542503efab4545fec3607aa
Moshfegh Hamedani <moshfegh@live.com.au>
2021-02-04 18:31:45 -0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker pull codecast/vidly_frontend
-docker pull codecast/vidly_backend
-docker service update vidly_frontend
-docker service update vidly_backend
+docker pull codecast/vidly-frontend
+docker pull codecast/vidly-backend
+docker service update vidly-frontend
+docker service update vidly-backend
