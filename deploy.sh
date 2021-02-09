34a50155e7b6c46832aa839436ea67f0d98fdcb3
Moshfegh Hamedani <moshfegh@live.com.au>
2021-02-08 16:50:13 -0800

    Remove unnecessary files.

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-docker pull codecast/vidly-frontend:latest
-docker pull codecast/vidly-backend:latest
-docker service update vidly_frontend
-docker service update vidly_backend
--- publish.sh
+++
@@ -1,4 +0,0 @@
-docker build -t codecast/vidly-frontend:latest frontend
-docker push codecast/vidly-frontend:latest
-docker build -t codecast/vidly-backend:latest backend
-docker push codecast/vidly-backend:latest
