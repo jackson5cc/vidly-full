9f7f243483ce584a9e1a505e8d60a11505af854d
Moshfegh Hamedani <moshfegh@live.com.au>
2021-02-10 18:04:29 -0800

    Add support for test env

---
+++ backend/config/test.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dbUri: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.oyurd.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`,
+};
