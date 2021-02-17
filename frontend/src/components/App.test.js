a1a159c33e8058df1dca576e941329b3044d0e66
Moshfegh Hamedani <moshfegh@live.com.au>
2021-02-17 09:57:24 -0800

    Write the first test for the React app.

--- frontend/src/components/App.test.js
+++ frontend/src/components/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockMovies = [
+  { _id: 1, title: "Movie 1" },
+  { _id: 2, title: "Movie 2" },
+  { _id: 3, title: "Movie 3" },
+];
+
+const server = setupServer(
+  rest.get("http://localhost:3001/movies", (req, res, ctx) => {
+    return res(ctx.json(mockMovies));
+  })
+);
+
+describe("App component", () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  test("renders the list of movies", async () => {
+    render(<App />);
+
+    await waitFor(() => screen.getAllByRole("listitem"));
+
+    const list = screen.getByRole("list");
+    const listItems = screen.getAllByRole("listitem");
+    expect(list).toBeInTheDocument();
+    expect(listItems.length).toEqual(mockMovies.length);
+  });
 });
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:3001";
-console.log("ENV:", process.env);
 
 const api = {
   get: (endpoint) => axios.get(baseUrl + endpoint),
